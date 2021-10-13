6455b7a1d0f72310f6535ab540b666f364fe246d
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2021-10-13 16:41:24 +0200

    more demo

--- py/demo.py
+++ py/demo.py
@@ -8,12 +8,12 @@ from scipy.optimize import minimize
 import mexpress
 
 
-def _run_method(f, method, jac, hess, n_runs=5):
+def _run_method(f, method, jac, hess, n_runs=3):
     elapseds = []
     xes = []
     for _ in range(n_runs):
         s = time()
-        x = minimize(f, [-1.0, -1.0], method=method, jac=jac, hess=hess)
+        x = minimize(f, [-1.0] * f.n_vars, method=method, jac=jac, hess=hess, options={"maxiter": 5000})
         elapsed = time() - s
         elapseds.append(elapsed)
         xes.append(x)
@@ -21,10 +21,12 @@ def _run_method(f, method, jac, hess, n_runs=5):
     elapsed = elapseds[med_idx]
     x = xes[med_idx]
     min_len = 12
-
+    x_ = ", ".join([f"{xi:8.5f}" for xi in x["x"]])
+    y_ = f"{f(x['x']):8.4f}"
     return (
         f"{method:{min_len}}",
-        f"({x['x'][0]:8.5f}, {x['x'][1]:8.5f})",
+        f"({x_})",
+        f"y {y_}",
         f"{elapsed:.6f} sec",
         f"jac {str(jac is not None):5}",
         f"hess {str(hess is not None):5}",
@@ -32,27 +34,28 @@ def _run_method(f, method, jac, hess, n_runs=5):
 
 
 def main(func_str):
-    f = mexpress.parse(func_str)
-    res = []
-
+    
     methods = [
-        ("CG", lambda f: f.grad, lambda f: None),
-        ("CG", lambda f: None, lambda f: None),
+        ("CG", lambda f: f.grad, lambda _: None),
+        ("CG", lambda _: None, lambda _: None),
         ("Newton-CG", lambda f: f.grad, lambda f: f.hess),
-        ("Newton-CG", lambda f: f.grad, lambda f: None),
+        ("Newton-CG", lambda f: f.grad, lambda _: None),
         ("trust-krylov", lambda f: f.grad, lambda f: f.hess),  # needs Hessian
         ("trust-ncg", lambda f: f.grad, lambda f: f.hess),  # needs Hessian
-        ("trust-exact", lambda f: f.grad, lambda f: f.hess),  # needs Hessian
-        ("BFGS", lambda f: f.grad, lambda f: None),
-        ("BFGS", lambda f: None, lambda f: None),
-        ("L-BFGS-B", lambda f: f.grad, lambda f: None),
-        ("L-BFGS-B", lambda f: None, lambda f: None),
-        ("Nelder-Mead", lambda f: None, lambda f: None),
-        ("SLSQP", lambda f: f.grad, lambda f: None),
+        # ("trust-exact", lambda f: f.grad, lambda f: f.hess),  # needs Hessian
+        ("BFGS", lambda f: f.grad, lambda _: None),
+        ("BFGS", lambda _: None, lambda _: None),
+        ("L-BFGS-B", lambda f: f.grad, lambda _: None),
+        ("L-BFGS-B", lambda _: None, lambda _: None),
+        ("Nelder-Mead", lambda _: None, lambda _: None),
+        ("SLSQP", lambda f: f.grad, lambda _: None),
         ("dogleg", lambda f: f.grad, lambda f: f.hess),
     ]
 
+    res = []
+
     for m in methods:
+        f = mexpress.parse(func_str)
         res = _run_method(f, m[0], m[1](f), m[2](f))
         print("   ".join(str(r) for r in res))
 
@@ -60,5 +63,5 @@ def main(func_str):
 if __name__ == "__main__":
     a = 1
     b = 100
-    rosen = f"({a}-x) ** 2 + {b}*(y-x ** 2) ** 2 "
-    main(rosen)
+    func = f"({a}-x) ** 2 + {b}*(y-x ** 2) ** 2 + (z - 7) ** 2 + (ρ + 5) ** 2"
+    main(func)
